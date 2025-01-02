86dddea6b799b367eaf241ca444a81f1b7c05ca1
CristianBlaz18 <danielblaz59@gmail.com>
2025-01-02 00:29:00 -0500

    Avanze Sprint 1

--- src/lib/apiVerificarEnviarCorreo.jsx
+++ src/lib/apiVerificarEnviarCorreo.jsx
@@ -1,12 +1,12 @@
-export const loginUser = async (email, password) => {
+export const verificarEnviarCorreo = async (email) => {
     const response = await fetch(
-      "https://ll6aenqwm9.execute-api.us-east-1.amazonaws.com/service/lambda-01-login",
+      "https://ll6aenqwm9.execute-api.us-east-1.amazonaws.com/service/lambda-04-enviarCodRecCorreo",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email}),
       }
     );
   
